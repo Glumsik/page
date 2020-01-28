b1e6640d583c63e2f6cdcfa529225b3d9b1d3f67
Glumsik <glumsik@icloud.com>
2020-01-28 17:36:41 +0300

    page
    page

---
+++ precache-manifest.be39916b130b14f787f392e1509e542e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d6827897d6e77e92e8ba9a410596ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dda2297066ba8e1f585",
+    "url": "/static/css/main.8fc25d6c.chunk.css"
+  },
+  {
+    "revision": "e53ebc9388f74324e350",
+    "url": "/static/js/2.c55dba80.chunk.js"
+  },
+  {
+    "revision": "95417c3b5d5a493e540822562736bc05",
+    "url": "/static/js/2.c55dba80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dda2297066ba8e1f585",
+    "url": "/static/js/main.59fe5ad5.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "aa0936093f846dc8d4996956b4a4d8bd",
+    "url": "/static/media/error.aa093609.svg"
+  }
+]);
